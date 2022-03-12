0fe350951ded2e04ed2b259fdbfe7760683a6c23
hugosenari <hugosenari@gmail.com>
2022-03-12 03:09:20 -0300

    chore: re index luna

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Lua convenience library for nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-luna-master.flake = false;
+  inputs.src-luna-master.owner = "smallfx";
+  inputs.src-luna-master.ref   = "refs/heads/master";
+  inputs.src-luna-master.repo  = "luna.nim";
+  inputs.src-luna-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-luna-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-luna-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
